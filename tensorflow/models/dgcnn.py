15c8a2be05488b9ab3d76bd87d22c9cdfe044989
Yang Jiang <yjiang@stackline.com>
2021-06-20 01:35:12 -0700

    upgrade exponential_decay to v2

--- tensorflow/models/dgcnn.py
+++ tensorflow/models/dgcnn.py
@@ -21,8 +21,8 @@ def placeholder_inputs(batch_size, num_point):
 
 def get_model(point_cloud, is_training, bn_decay=None):
     """ Classification PointNet, input is BxNx3, output Bx40 """
-    batch_size = point_cloud.get_shape()[0].value
-    num_point = point_cloud.get_shape()[1].value
+    batch_size = point_cloud.get_shape()[0]
+    num_point = point_cloud.get_shape()[1]
     end_points = {}
     k = 20
 
